a3e2f2cc30c19fde20a0a5e5003d761bf16d3588
Vitaliy Doroshenko <tadoko@bk.ru>
2024-12-07 13:35:19 +0300

    return config

--- config.py
+++ config.py
@@ -7,7 +7,7 @@ class Settings(BaseSettings):
     app_name: str = "AutoNanny"
     ssl_certfile: str = "fullchain.pem"
     ssl_keyfile: str = "privkey.pem"
-    port: int = 4443
+    port: int = 443
     log_level: str = "debug"
     report_file_path: str = "./"
 
